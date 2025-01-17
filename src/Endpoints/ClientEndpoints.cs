a7f19c06c6aea01ea19ec641f59feeb4a138d72c
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-01-17 12:38:14 -0300

    client endpoint segregation

---
+++ src/Endpoints/ClientEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using API.Middleware;
+using API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Endpoints;
+
+public static class ClientEndpoints
+{
+    public static void CreateClientEndpoints(this WebApplication app)
+    {
+        Post(app);
+        Get(app);
+    }
+
+    private static void Post(this WebApplication app)
+    {
+        app.MapPost("/client", async ([FromBody] ClientDto dto, [FromServices] ClientMid mid) =>
+                {
+                    await mid.CreateIfIsValid(dto);
+                })
+        .WithName("CreateClient")
+        .WithOpenApi();
+    }
+    private static void Get(this WebApplication app)
+    {
+        app.MapPost("/client/{cliendId}", async ([FromBody] ClientDto dto, [FromQuery] int cliendId) =>
+        {
+            return "RETORNANDO CLIENTE";
+        })
+        .WithName("GetClient")
+        .WithOpenApi();
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,8 @@
 using API.Configuration;
-using API.Middleware;
-using API.Models.Dto;
-using Microsoft.AspNetCore.Mvc;
-using System.Text.Json.Serialization;
+using API.Endpoints;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,29 +10,13 @@ builder.LoadAppSettings();
 builder.LoadDataBaseConnection();
 builder.LoadDependencies();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-//===MAPEAMENTO DAS APIS
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapPost("/client", async ([FromBody ]ClientDto dto, [FromServices] ClientMid mid) =>
-{
-    await mid.CreateIfIsValid(dto);
-})
-.WithName("CreateClient")
-.WithOpenApi();
-//===FIM MAPEAMENTO DAS APIS
+app.CreateClientEndpoints();
 
 app.Run();
