f9354e6739ca0603b29bd70cb3f54496972f2d77
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-01-22 11:18:19 -0300

    create date properties for partnerships

--- src/Models/Entities/Partnerships.cs
+++ src/Models/Entities/Partnerships.cs
@@ -6,5 +6,8 @@ public class Partnerships : Entitie
 {
     public List<Client> Nominees { get; set; } = [];
     public List<Client> Referrer { get; set; } = [];
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateOnly ValidUntil { get; set; } = DateOnly.FromDateTime(DateTime.Now.AddYears(1));
     public EPartnershipStatus PartnershipStatus { get; private set; }
 }
